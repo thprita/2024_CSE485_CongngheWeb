92f2b2b4a84713e31e8b303c3d74992496e3bd62
thprita <phuongcun35@gmail.com>
2024-02-19 10:34:10 +0700

    test 1

--- project12/index.php
+++ project12/index.php
@@ -8,6 +8,23 @@
     <title>Document</title>
 </head>
 <body>
-
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+echo '<nav><ul>';
+foreach ($navItems as $item) {
+    echo "<li>$item</li>";
+}
+echo '</ul></nav>';
+?>
 </body>
 </html>
